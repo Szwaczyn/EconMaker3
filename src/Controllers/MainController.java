8a530c5cbba0f14b2af9e426e0ee0feee26e9744
Michal Szaczyk <michal.szwaczyk@outlook.com>
2018-09-17 12:30:16 +0200

    Dodana menu

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -1,6 +1,32 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 public class MainController
 {
+    @FXML
+    private StackPane mainStackPane;
 
+    @FXML
+    public void initialize()
+    {
+        inicializujMenu();
+    }
+
+    public void inicializujMenu()
+    {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/layoutFXML/startMenu.fxml"));
+        try {
+            Pane mainPaneMenu = loader.load();
+            startMenuController smc = loader.getController();
+            smc.setMainController(this);
+            mainStackPane.getChildren().clear();
+            mainStackPane.getChildren().add(mainPaneMenu);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- src/Controllers/startMenuController.java
+++ src/Controllers/startMenuController.java
@@ -1,4 +1,93 @@
 package Controllers;
 
-public class startMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class startMenuController
+{
+
+    private MainController mainControllerVar;
+
+    Image button = new Image("/resources/images/button.png");
+    Image buttonUnderCursor = new Image("/resources/images/buttonUnderCursor.png");
+
+    @FXML
+    ImageView position_1 = new ImageView();
+    @FXML
+    ImageView position_2 = new ImageView();
+    @FXML
+    ImageView position_3 = new ImageView();
+    @FXML
+    ImageView position_4 = new ImageView();
+    @FXML
+    ImageView position_5 = new ImageView();
+
+
+    @FXML
+    public void chooisePositionMenu()
+    {
+        if(position_1.getImage() == buttonUnderCursor)
+        {
+            System.out.println("Pozycja 1");
+        }
+        else if(position_2.getImage() == buttonUnderCursor)
+        {
+            System.out.println("Pozycja 2");
+        }
+        else if(position_3.getImage() == buttonUnderCursor)
+        {
+            System.out.println("Pozycja 3");
+        }
+        else if(position_4.getImage() == buttonUnderCursor)
+        {
+            System.out.println("Pozycja 4");
+        }
+        else if(position_5.getImage() == buttonUnderCursor)
+        {
+            System.out.println("Pozycja 5");
+        }
+    }
+
+    @FXML
+    public void drawChooseMenu_position_1()
+    {
+        position_1.setImage(buttonUnderCursor);
+    }
+    @FXML
+    public void drawChooseMenu_position_2()
+    {
+        position_2.setImage(buttonUnderCursor);
+    }
+    @FXML
+    public void drawChooseMenu_position_3()
+    {
+        position_3.setImage(buttonUnderCursor);
+    }
+    @FXML
+    public void drawChooseMenu_position_4()
+    {
+        position_4.setImage(buttonUnderCursor);
+    }
+    @FXML
+    public void drawChooseMenu_position_5()
+    {
+        position_5.setImage(buttonUnderCursor);
+    }
+
+    @FXML
+    public void leaveCoursorButton()
+    {
+        position_1.setImage(button);
+        position_2.setImage(button);
+        position_3.setImage(button);
+        position_4.setImage(button);
+        position_5.setImage(button);
+    }
+
+    public void setMainController(MainController msc)
+    {
+        this.mainControllerVar = msc;
+    }
 }
