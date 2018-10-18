6be71dfa25035d343e91f4f13db276472815f6bd
Michal Szaczyk <michal.szwaczyk@outlook.com>
2018-10-18 13:38:34 +0200

    Stworzenie bug trackera

--- src/Controllers/startCreateAccountUserControllers/CreateAccountUserController.java
+++ src/Controllers/startCreateAccountUserControllers/CreateAccountUserController.java
@@ -1,6 +1,9 @@
 package Controllers.startCreateAccountUserControllers;
 
 import Controllers.MainController;
+import hoodStuff.BugTracker;
+import hoodStuff.LanguageEngine;
+import hoodStuff.sqlConnection;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,6 +15,9 @@ import javafx.scene.control.TextField;
  */
 public class CreateAccountUserController
 {
+    LanguageEngine translation = new LanguageEngine();
+    sqlConnection sqlConnect = new sqlConnection();
+
     @FXML
     Button buttonCreate = new Button();
     @FXML
@@ -40,7 +46,62 @@ public class CreateAccountUserController
     @FXML
     PasswordField textPassword = new PasswordField();
 
+    @FXML
+    public void createUser()
+    {
+        if(checkTheCorrectness(textLogin.getText(), textPassword.getText(), textQuestion.getText(), textAnswer.getText()))
+        {
+            procedureSendUserToDatabase();
+        }
+        else
+        {
+            //TODO System error
+            System.out.println("Nie mozna dodać użytkownika");
+        }
 
+    }
+
+    private void procedureSendUserToDatabase()
+    {
+        System.out.println("Dodawanie użytkownika");
+    }
+
+    private boolean checkTheCorrectness(String login, String password, String question, String answer)
+    {
+        boolean correctnss = false;
+        BugTracker bug = new BugTracker(correctnss);
+
+        if(login.equals("lo"))
+        {
+            correctnss = true;
+            bug.show();
+        }
+        bug.show();
+        return correctnss;
+    }
+
+    @FXML
+    public void clearTextField()
+    {
+        textLogin.setText("");
+        textPassword.setText("");
+        textQuestion.setText("");
+        textAnswer.setText("");
+    }
+
+    public void initialize()
+    {
+        buttonCreate.setText(translation.setUpLanguage(26));
+        buttonClear.setText(translation.setUpLanguage(27));
+        position_1.setText(translation.setUpLanguage(6));
+        labelLogin.setText(translation.setUpLanguage(22));
+        labelPassword.setText(translation.setUpLanguage(23));
+        labelQuestion.setText(translation.setUpLanguage(24));
+        labelAnswer.setText(translation.setUpLanguage(25));
+        labelTitle.setText(translation.setUpLanguage(28));
+
+        textLogin.setText("");
+    }
 
     private MainController mainControllerVar;
 
---
+++ src/hoodStuff/BugTracker.java
@@ -0,0 +1,59 @@
+package hoodStuff;
+
+/**
+ * Created $(DATE)
+ */
+public class BugTracker
+{
+    Object x;
+    int iterator;
+
+    public BugTracker(int x)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(long x, int xz)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(String x)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(double x)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(float x)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(boolean x)
+    {
+        this.x = x;
+        this.iterator = 1;
+    }
+
+    public BugTracker(){ this.x = 0; this.iterator = 1; }
+
+    public void showCondition(Object object, String nameOfObject)
+    {
+        System.out.println("Aktualny stan obiektu " + nameOfObject + " to: " + object);
+    }
+
+    public void show()
+    {
+        System.out.println("Aktualny stan obiektu to: " + x + "  // wystąpienie numer - " + iterator);
+        iterator++;
+    }
+}
