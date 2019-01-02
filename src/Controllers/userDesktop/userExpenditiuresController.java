39963fdcd18a13c40288ce8a0b6eda58c064299d
unknown <michal.szwaczyk@outlook.com>
2019-01-02 22:03:29 +0100

    Utworzenie kontrolek dla okna wydatki

--- src/Controllers/userDesktop/userExpenditiuresController.java
+++ src/Controllers/userDesktop/userExpenditiuresController.java
@@ -4,6 +4,7 @@ import Controllers.ClassController;
 import builder.ChangeWindowBuilder;
 import hoodStuff.ChangeWindow;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 /**
  * Created $(DATE)
@@ -24,4 +25,46 @@ public class userExpenditiuresController extends ClassController
 
         window.changeWindow();
     }
+
+    /**
+     *  Controls - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     */
+
+    @FXML
+    Button buttonReturn = new Button();
+    @FXML
+    Button buttonSaveExpenditiure = new Button();
+    @FXML
+    Button buttonClearExpenditiure = new Button();
+
+    @FXML
+    Label labelAccount = new Label();
+    @FXML
+    Label labelNameOfExpenditiure = new Label();
+    @FXML
+    Label labelValueOfExpenditiure = new Label();
+    @FXML
+    Label labelDataOfExpenditiure = new Label();
+    @FXML
+    Label labelSetBoudgetOfExpenditiure = new Label();
+
+    @FXML
+    CheckBox checkBoxCategoryOfExpenditiure = new CheckBox();
+    @FXML
+    CheckBox CheckBoxBoudgetOfExpenditiure = new CheckBox();
+
+    @FXML
+    TextField textNameOfExpenditiure = new TextField();
+    @FXML
+    TextField textValuieOfExpenditiure = new TextField();
+
+    @FXML
+    ChoiceBox choiceBoxAccount = new ChoiceBox();
+    @FXML
+    ChoiceBox choiceBoxCategoryOfExpenditiure = new ChoiceBox();
+    @FXML
+    ChoiceBox choiceBoxSetBoudgetOfExpenditiure = new ChoiceBox();
+
+    @FXML
+    DatePicker datePickerOfExpenditiure = new DatePicker();
 }
