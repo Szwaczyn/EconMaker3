f6f5c53df066146239e4bca9800a0a35371ecebb
unknown <michal.szwaczyk@outlook.com>
2019-01-02 22:18:45 +0100

    Translator dla okna wydatków

--- src/Controllers/userDesktop/userExpenditiuresController.java
+++ src/Controllers/userDesktop/userExpenditiuresController.java
@@ -3,6 +3,7 @@ package Controllers.userDesktop;
 import Controllers.ClassController;
 import builder.ChangeWindowBuilder;
 import hoodStuff.ChangeWindow;
+import hoodStuff.LanguageEngine;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -26,9 +27,35 @@ public class userExpenditiuresController extends ClassController
         window.changeWindow();
     }
 
+
+    /**
+     *  Initialize - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     */
+
+    public void initialize()
+    {
+        setUpLanguage();
+    }
+
+    private void setUpLanguage()
+    {
+        buttonReturn.setText(translation.setUpLanguage(6));
+        buttonSaveExpenditiure.setText(translation.setUpLanguage(9));
+        buttonClearExpenditiure.setText(translation.setUpLanguage(27));
+        checkBoxCategoryOfExpenditiure.setText(translation.setUpLanguage(88));
+        checkBoxBoudgetOfExpenditiure.setText(translation.setUpLanguage(42));
+        labelSetBoudgetOfExpenditiure.setText(translation.setUpLanguage(110));
+        labelAccount.setText(translation.setUpLanguage(65));
+        labelNameOfExpenditiure.setText(translation.setUpLanguage(107));
+        labelValueOfExpenditiure.setText(translation.setUpLanguage(108));
+        labelDataOfExpenditiure.setText(translation.setUpLanguage(109));
+    }
+
     /**
      *  Controls - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      */
+
+    LanguageEngine translation = new LanguageEngine();
 
     @FXML
     Button buttonReturn = new Button();
@@ -51,7 +78,7 @@ public class userExpenditiuresController extends ClassController
     @FXML
     CheckBox checkBoxCategoryOfExpenditiure = new CheckBox();
     @FXML
-    CheckBox CheckBoxBoudgetOfExpenditiure = new CheckBox();
+    CheckBox checkBoxBoudgetOfExpenditiure = new CheckBox();
 
     @FXML
     TextField textNameOfExpenditiure = new TextField();
